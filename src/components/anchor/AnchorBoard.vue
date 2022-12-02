944b68f976aa716da7b0343f02b2d245cde93e99
broguyman <broguyman@gmail.com>
2022-12-01 20:28:01 -0600

    1 file modified, 1 added

---
+++ src/components/anchor/AnchorBoard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="board"></div>
+</template>
+
+<style scoped>
+.board {
+  background-color: #000;
+  width: 100%;
+  height: 100%;
+}
+</style>
